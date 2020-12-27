409d4b5ad396f85fd1a2753b1cbfb4c365d670fa
Dayne Dewar <daynedewar@yahoo.com>
2020-12-27 03:05:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,8 @@ require 'pry'
 def using_concat(array1, array2)
   counter = 0
   
-  while array2 do 
+  while counter do 
     array1.push(array2[counter])
-  binding.pry
+    counter += 1
+  end
 end
